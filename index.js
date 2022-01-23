3d3c8b213e9f4068d8a1b05c1d83c0da17f44a1b
mrNorii <ksabiron03@gmail.com>
2022-01-23 17:37:28 +0100

    scraping title and url from trojmiasto.pl

--- index.js
+++ index.js
@@ -7,11 +7,22 @@ const application = express()
 
 //Scraping here
 
-axios('https://www.x-kom.pl')
+axios('https://www.trojmiasto.pl/wiadomosci/')
     .then(response => {
         const html = response.data
-        console.log(html)
-    })
+        const a = cheerio.load(html)
+        const table = []
+
+        a('.color04', html).each(function() {
+            const title = a(this).text()
+            const url = a(this).attr('href')
+            table.push({
+                title, 
+                url
+            })
+        })
+        console.log(table)       
+    }).catch(err => console.log(err))
 
 //End of Scraping
 
