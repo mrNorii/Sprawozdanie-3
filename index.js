b8b0f0560f3e8ad87a66c1155b190cc0c47a62d4
mrNorii <ksabiron03@gmail.com>
2022-01-23 16:40:34 +0100

    made few const and start app

---
+++ index.js
@@ -0,0 +1,8 @@
+const axios = require('axios')
+const express = require('express')
+const cheerio = require('cheerio')
+const PORT = 8000
+
+const app = express()
+
+app.listen(PORT, () => console.log(`PORT: ${PORT}`))
